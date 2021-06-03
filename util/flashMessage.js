7fb0cded2f5620e489a8ae112d325a660dcd727f
ckymn <cokyamanmuhammet@gmail.com>
2021-06-03 21:11:04 +0300

    CF-12 code fixed

--- util/flashMessage.js
+++ util/flashMessage.js
@@ -1,38 +1,3 @@
-// module.exports = [
-//   {
-//     type: "alert alert-success",
-//     message: "Blog Create Successfully",
-//   },
-//   {
-//     type: "alert alert-danger",
-//     message: "Blog Create Incorrect",
-//   },
-//   {
-//     type: "alert alert-danger",
-//     message: "You Have To Login",
-//   },
-//   {
-//     type: "alert alert-danger",
-//     message: "You Have To SignUp",
-//   },
-//   {
-//     type: "alert alert-danger",
-//     message: "Have Already email . You Should Other Email or You should Login",
-//   },
-//   {
-//     type: "alert alert-success",
-//     message: "SignUp Success",
-//   },
-//   {
-//     type: "alert alert-success",
-//     message: "Category Create Successfully",
-//   },
-//   {
-//     type: "alert alert-danger",
-//     message: "Category Create Incorrect",
-//   },
-// ];
-
 const sendMessage = (type, message) => {
   return {
     type,
