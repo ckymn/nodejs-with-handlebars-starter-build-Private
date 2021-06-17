5bb846e22275505b8f913907d05a917567c0783c
ckymn <cokyamanmuhammet@gmail.com>
2021-06-17 15:03:45 +0300

    CF-21 category aggregation is done :)

--- modules/get/route/blog.js
+++ modules/get/route/blog.js
@@ -5,7 +5,24 @@ const Users = require("../../user/model");
 const route = async(req,res) => {
 	const { body, params} = req;
 	let _post = await Post.find().populate({ path: "author",model: Users}).sort({createdAt: -1});
-	let _category = await Category.find().sort({createdAt: -1});
+	let _category = await Category.aggregate([
+		{
+			$lookup:{
+				from: "posts",
+				localField: "_id",
+				foreignField: "category",
+				as: "posts"
+			},
+		},
+		{
+			$project:{
+				_id: 1,
+				name: 1,
+				num_of_posts: { $size: "$posts" }
+			}
+		}
+	]);	
+	console.log(_category);
 	if(!_post)
 		return res.status(404).render("addpost");
 	return res.render("blog", {posts: _post, category: _category});
